1670368551f876584cf0a0e1dd6cd6b64ff16077
OmSantoshBhujbal <omsb22hcompe@student.mes.ac.in>
2023-08-26 12:09:05 +0530

    First Java Programs

---
+++ EmployeeDetails.java
@@ -0,0 +1,11 @@
+
+public class EmployeeDetails
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello Everynyan");
+		System.out.println("How are You?");
+		System.out.println("Fine Sankyou");
+	}
+
+}
---
+++ Operations.java
@@ -0,0 +1,16 @@
+public class Operations {
+    public static void main(String[] args) {
+        float a,b,sum,multiply,subtract,division;
+        a=10;
+        b=15;
+        sum=a+b;
+        System.out.println("sum is\t"+sum);
+        multiply=a*b;
+        System.out.println("multiplication is\t "+multiply);
+        subtract=a-b;
+        System.out.println("subtraction is\t"+subtract);
+        division=a/b;
+        System.out.println("division is\t"+division);
+    }
+    
+}
