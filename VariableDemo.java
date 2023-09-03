de9acd544a498d2542599b0bd35608ae3aabd7f7
OmSantoshBhujbal <omsb22hcompe@student.mes.ac.in>
2023-09-03 11:56:58 +0530

    VariableDemo

--- VariableDemo.java
+++ VariableDemo.java
@@ -9,6 +9,7 @@ public class VariableDemo {
         float f=10.0f;
         double d=10.0d;
         a=true;
+        String str="om"; 
         System.out.println("boolean value is "+a);
         System.out.println("char value is "+c);
         System.out.println("short value is"+s1);
@@ -16,6 +17,7 @@ public class VariableDemo {
         System.out.println("long value is"+l);
         System.out.println("float value is"+f);
         System.out.println("double value is "+d);
+        System.out.println("Name is"+str);
         System.out.println("byte value is"+b);    
     }
 }
