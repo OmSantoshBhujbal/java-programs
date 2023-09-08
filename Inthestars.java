c700a4a260fcdbf4dea55a60c69526e1b5cbc50c
OmSantoshBhujbal <omsb22hcompe@student.mes.ac.in>
2023-09-08 22:34:05 +0530

    JavaPrograms

---
+++ Inthestars.java
@@ -0,0 +1,9 @@
+public class Inthestars {
+    public static void main(String[] args) {
+        System.out.println("Im still holding on to everything that's dead and gone ");
+        System.out.println("I dont wanna say goodbye cause this one means foreverrr");
+        System.out.println("And now you're in the stars and six feet never felt so far");
+        System.out.println("Here i am alone between the heaven and the embers");
+    }
+    
+}
