f894913cb12f8d148a0d96f6651d03ac6ff98afa
OmSantoshBhujbal <omsb22hcompe@student.mes.ac.in>
2023-09-08 22:33:24 +0530

    Newprograms

---
+++ AreaofCircle.java
@@ -0,0 +1,9 @@
+public class AreaofCircle {
+    public static void main(String[] args) {
+        double a,area,pi=3.14;
+        a=10;
+        area = pi*a*a;
+        System.out.println("Area of the given circle is :"+area);
+    }
+    
+}
