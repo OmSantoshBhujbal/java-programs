6335a74b9079b1fbcef7fb57f6ce3f4c13226d4b
OmSantoshBhujbal <omsb22hcompe@student.mes.ac.in>
2023-12-06 09:18:10 +0530

    java programs

--- PageSizeViewer.java
+++ PageSizeViewer.java
@@ -4,72 +4,71 @@ public class PageSizeViewer {
         int lengthA0,breadthA0;
         lengthA0 = 1189;
         breadthA0 = 841;
-        System.out.println("Length of the page is"+ lengthA0 +"mm");
-        System.out.println("Breadth of the page is"+breadthA0 +"mm");
+        System.out.println("Length of page is"+ lengthA0 +"mm");
+        System.out.println("Breadth of page is"+breadthA0 +"mm");
         } 
         void displayA1(){
         int lengthA1,breadthA1;
         lengthA1 = 594;
         breadthA1 = 841;
-        System.out.println("Length of the page is"+ lengthA1 +"mm");
-        System.out.println("Breadth of the page is"+breadthA1 +"mm");
+        System.out.println("Length of page is"+ lengthA1 +"mm");
+        System.out.println("Breadth of page is"+breadthA1 +"mm");
         }
         void displayA2(){
         int lengthA2,breadthA2;
         lengthA2 = 595;
         breadthA2 = 420;
-        System.out.println("Length of the page is"+ lengthA2 +"mm");
-        System.out.println("Breadth of the page is"+breadthA2 +"mm");
+        System.out.println("Length of page is"+ lengthA2 +"mm");
+        System.out.println("Breadth of page is"+breadthA2 +"mm");
         }
         void displayA3(){
         int lengthA3,breadthA3;
         lengthA3 = 297;
         breadthA3 = 420;
-        System.out.println("Length of the page is"+ lengthA3 +"mm");
-        System.out.println("Breadth of the page is"+breadthA3 +"mm");
+        System.out.println("Length of page is"+ lengthA3 +"mm");
+        System.out.println("Breadth of page is"+breadthA3 +"mm");
         }
         void displayA4(){
         int lengthA4,breadthA4;
         lengthA4 = 210;
         breadthA4 = 298;
-        System.out.println("Length of the page is"+ lengthA4 +"mm");
-        System.out.println("Breadth of the page is"+breadthA4 +"mm");
+        System.out.println("Length of page is"+ lengthA4 +"mm");
+        System.out.println("Breadth of page is"+breadthA4 +"mm");
         }
         void displayA5(){
         int lengthA5,breadthA5;
         lengthA5 = 148;
         breadthA5 = 276;
-        System.out.println("Length of the page is"+ lengthA5 +"mm");
-        System.out.println("Breadth of the page is"+breadthA5 +"mm");
+        System.out.println("Length of page is"+ lengthA5 +"mm");
+        System.out.println("Breadth of page is"+breadthA5 +"mm");
         }
         void displayA6(){
         int lengthA6,breadthA6;
         lengthA6 = 150;
         breadthA6 = 105;
-        System.out.println("Length of the page is"+ lengthA6  +"mm");
-        System.out.println("Breadth of the page is"+breadthA6 +"mm");
+        System.out.println("Length of page is"+ lengthA6  +"mm");
+        System.out.println("Breadth of page is"+breadthA6 +"mm");
         }
         void displayA7(){
         int lengthA7,breadthA7;
         lengthA7 = 74;
         breadthA7 = 106;
-        System.out.println("Length of the page is"+ lengthA7 +"mm");
-        System.out.println("Breadth of the page is"+breadthA7 +"mm");   
+        System.out.println("Length of page is"+ lengthA7 +"mm");
+        System.out.println("Breadth of page is"+breadthA7 +"mm");   
         }    
         void displayA8(){
         int lengthA8,breadthA8;
         lengthA8 = 76;
         breadthA8 = 52; 
-        System.out.println("Length of the page is"+ lengthA8 +"mm");
-        System.out.println("Breadth of the page is"+breadthA8 +"mm");   
+        System.out.println("Length of page is"+ lengthA8 +"mm");
+        System.out.println("Breadth of page is"+breadthA8 +"mm");   
         }
     public static void main(String[] args) {
         PageSizeViewer pageSizeViewer = new PageSizeViewer();
         int Pagetype;
-        Scanner S = new Scanner(System.in);
+        Scanner Sc = new Scanner(System.in);
         System.out.print("Enter Page Type :A");
-        Pagetype = S.nextInt();
-        
+        Pagetype = Sc.nextInt();
         
         if(+Pagetype == 0)
         {
@@ -111,6 +110,6 @@ public class PageSizeViewer {
         {
         System.out.println("Invalid Page Type");
         }
-        S.close();
+        Sc.close();
     }   
     }
